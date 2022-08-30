37b59b7630dbfa9165ebc4550cce82185bb69c91
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-08-30 13:35:16 +0300

    probably got it this time, 11.9

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,7 @@
 describe("Pokedex", function () {
 	it("front page can be opened", function () {
 		cy.visit("http://localhost:5000");
-		cy.contains("ivysaur");
-		cy.contains(
-			"Pokémon and Pokémon character names are trademarks of Nintendo.",
-		);
+		cy.contains("pikachu");
 	});
 	it("Pokemon called Charmander can be opened and has right content", function () {
 		cy.get("#app")
